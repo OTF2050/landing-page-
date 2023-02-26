1a525793e5e28a595f01cd96f32dc0c4e64f1df1
OTF2050 <121712082+OTF2050@users.noreply.github.com>
2023-02-26 22:43:35 +0400

    first commit

---
+++ JS/index.js
@@ -0,0 +1,38 @@
+var countDownDate=new Date();
+countDownDate=new Date(countDownDate.getFullYear()+1,countDownDate.getMonth()- 6,01)
+var x = setInterval(function(){
+    var now = new Date();
+    var  diff =countDownDate-now;
+
+var month = Math.floor(diff/(1000*60*60*24*30));
+var days= Math.floor(diff%(1000*60*60*24*30)/(1000*60*60*24));
+var hours= Math.floor(diff%(1000*60*60*24)/(1000*60*60));
+var minute= Math.floor(diff%(1000*60*60) /(1000*60));
+var second= Math.floor(diff%(1000*60) /(1000));
+
+document.getElementById("months").innerHTML=month;
+document.getElementById("days").innerHTML=days;
+document.getElementById("hours").innerHTML=hours;
+document.getElementById("minutes").innerHTML=minute;
+document.getElementById("seconds").innerHTML= second;
+},1000);
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+      form.addEventListener('submit', event => {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+  
+        form.classList.add('was-validated')
+      }, false)
+    })
+  })();
+  
